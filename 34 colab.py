c08966bfda4d94ac582aa56cb824197bff6315ac
Evelyn Karoline Burgel Schonton <130072426+evelynkbs@users.noreply.github.com>
2024-06-25 15:03:04 -0300

    Add files via upload

---
+++ 34 colab.py
@@ -0,0 +1,75 @@
+#Exercício 1
+print("Exercício 1")
+lista = [12,24,32,48,54,63,71]
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print(lista[3])
+print(lista[4])
+print(lista[5])
+print(lista[6])
+
+
+#Exercício 2
+print("Exercício 2")
+tupla = (16,22,35,43,98,64,71)
+print(tupla [0])
+print(tupla[1])
+print(tupla[2])
+print(tupla[3])
+print(tupla[4])
+print(tupla[5])
+print(tupla[6])
+
+
+#Exercício 3
+print("Exercício 3")
+lista = [1,2,3]
+lista.append(5)
+lista.append(8)
+lista.append(10)
+lista.append(-8)
+lista.append(60)
+lista.append(-37)
+print(lista)
+
+
+#Exercício 4
+print("Exercício 4")
+lista = [1,2,3,7,9,11,15]
+lista.remove(2)
+lista.remove(3)
+lista.remove(9)
+lista.remove(11)
+print(lista)
+
+
+#Exercício 5
+print("Exercício 5")
+nomes = ["Ana", "João", "Maria", "Pedro"]
+numeros = [1, 2, 3, 4, 5]
+frutas = ["maçã", "banana"]
+precos = [10.99, 20.49, 5.75, 8.30, 2.30, 8.30, 2]
+valores_booleanos = [True]
+print(len(nomes))
+print(len(numeros))
+print(len(frutas))
+print(len(precos))
+print(len(valores_booleanos))
+
+
+#Exercício 6
+print("Exercício 6")
+lista_int = [1, 2, 3, 4, 5]
+lista_float = [1.1, 2.2, 3.3, 4.4]
+outra_lista_int = [10, 20, 30, 40, 50, 60]
+outra_lista_string = ["dog", "cat", "mouse", "rabbit"]
+for item in lista_int:
+  print(item)
+for item in lista_float:
+  print(item)
+for item in outra_lista_int:
+  print(item)
+for item in outra_lista_string:
+  print(item)
+
